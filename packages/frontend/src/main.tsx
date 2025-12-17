1560359fb823125818feacf8d5b7c8ccf20aa66b
Tupynambá <tupynambalucas@gmail.com>
2025-12-16 23:44:24 -0300

    tailwind update

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -7,7 +7,7 @@ import '@fontsource/inter/500.css';
 import '@fontsource/inter/600.css';
 import '@fontsource/inter/700.css';
 import '@fontsource/inter/900.css';
-import './index.css';
+import './main.css';
 import App from './App';
 
 const rootElement = document.getElementById('root');
